3413272ac09db67a1e8fecc6cabe717a4fbe7fc1
grant-kun <geedub602@gmail.com>
2022-06-17 22:35:50 -0500

    added localhost support

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-var http = require('http');
+
 var fs = require('fs');
-
+//express 
+var express = require('express');
+var app = express();
+//this dir
+var dir = __dirname;
 const PORT=3000; 
-
+let t = dir + './crates.json'
 fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.write(html);  
-    }).listen(PORT);
+//listen
+app.listen(PORT, function () {
+    console.log('listening on port ' + PORT);
+});
+//on request serve index.html
+app.get('/', function (req, res) {
+    res.sendFile('index.html', {root: __dirname })
+})
+app.get('/crates.json', function (req, res) {
+    res.sendFile('crates.json', {root: __dirname })
+})
+    
 });
