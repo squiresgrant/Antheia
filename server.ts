76f2ee4fec6155165f497beacb1506a0708d7f56
grant-kun <geedub602@gmail.com>
2022-06-22 19:00:05 -0500

    general updates

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
-const PORT = 8008 || process.env.PORT;; 
+const PORT = 8008 || process.env.PORT;
 fs.readFile('./index.html', function (err:any, html:any) {
 app.listen(PORT, function () {
     console.log('listening! http://localhost:' + PORT);
@@ -11,4 +11,4 @@ app.get('/', function (req:any, res:any) {
 })
 app.get('/crates.json', function (req:any, res:any) {
     res.sendFile('crates.json', {root: __dirname })
-})})
+})});
