5930efd3577487ba38dc13500e90a790bba603b3
grant-kun <geedub602@gmail.com>
2022-06-23 18:25:56 -0500

    scheduled (not really) update

--- server.ts
+++ server.ts
@@ -1,14 +1,12 @@
-var fs = require('fs');
 var express = require('express');
 var app = express();
-const PORT = 8008 || process.env.PORT;
-fs.readFile('./index.html', function (err:any, html:any) {
+const PORT = 8008;
 app.listen(PORT, function () {
-    console.log('listening! http://localhost:' + PORT);
+    console.log('listening! http://localhost:' + PORT + ' | http://' + require("ip").address() + ':' + PORT);
 });
 app.get('/', function (req:any, res:any) {
     res.sendFile('index.html', {root: __dirname })
 })
 app.get('/crates.json', function (req:any, res:any) {
     res.sendFile('crates.json', {root: __dirname })
-})});
+});
